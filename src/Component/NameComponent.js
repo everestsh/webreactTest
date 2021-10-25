c652c167f8cd2b139c6745a19ba2849f838e159c
sooof <sooof@me.com>
2021-10-25 15:26:37 -0700

    index_u3w31d1_this.state2.js

---
+++ src/Component/NameComponent.js
@@ -0,0 +1,5 @@
+import React  from 'react'
+
+const Name = props => <h4>My name is {props.propsMessage}</h4>;
+
+export default Name;
--- src/u3/w3.1/index_u3w31d1_this.state1.js
+++ src/u3/w3.1/index_u3w31d1_this.state1.js
@@ -1,23 +1,19 @@
 import React  from 'react'
 import { render } from 'react-dom'
-import Message from  './Component/MessageComponent'
-import Name from  './Component/NameComponent'
-
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: "Hello form props",
+      message: "Hello form state",
       name : "Ryan"
     };
   }
-  
+
   render() {
     return (
       <div>
-        <Message propsMessage={this.state.message}/>
-        <Name propsMessage={this.state.name}/>
+        <h1> {this.state.message}, My name is {this.state.name}</h1>
       </div>
     );
   }
