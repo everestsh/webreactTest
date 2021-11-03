8fdf5816514ff0794536e2211ae6c9f58f16cfa0
sooof <sooof@me.com>
2021-11-03 16:10:52 -0700

    redux-store-ls_class_test The simplest < reducer code > test

--- U3/w32d3/redux-store-ls_class_test/src/components/Title.js
+++ U3/w32d3/redux-store-ls_class_test/src/components/Title.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 class Title extends React.Component {
 
-    
+
+
     render() {
         return (
         <div className="App">
-            <h1>Title Test</h1>
+            <h1>{this.props.title}</h1>
             
         </div>
         );
     }
 
   }
-  
-  export default Title;
+  const mapStateToProps = state =>{
+      console.log("currentState", state)
+      return {
+        title: state.title
+      }
+  }
+// export default Title;
+export default connect(
+    mapStateToProps, 
+    {})(Title);
   
