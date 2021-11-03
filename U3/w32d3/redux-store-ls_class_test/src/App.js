1b18992cd1eb15b9d18c333494cde4f5dce67717
sooof <sooof@me.com>
2021-11-03 15:53:14 -0700

    redux-store-ls_class_test Title class first test ok

--- U3/w32d3/redux-store-ls_class_test/src/App.js
+++ U3/w32d3/redux-store-ls_class_test/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
+import Title from './components/Title'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Title from Redux Store</h1>
+      <Title />
     </div>
   );
 }
---
+++ U3/w32d3/redux-store-ls_class_test/src/components/Title.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Title extends React.Component {
+
+    
+    render() {
+        return (
+        <div className="App">
+            <h1>Title Test</h1>
+            
+        </div>
+        );
+    }
+
+  }
+  
+  export default Title;
+  
