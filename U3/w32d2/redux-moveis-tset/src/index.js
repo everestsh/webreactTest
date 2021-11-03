e32fc9098f91f116eaec705e438c33b47952f054
sooof <sooof@me.com>
2021-11-03 14:20:42 -0700

    index.js const reducer = (state , action) => {

--- U3/w32d2/redux-moveis-tset/src/index.js
+++ U3/w32d2/redux-moveis-tset/src/index.js
@@ -4,10 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+import { Provider} from 'react-redux';
+// import reducer from './reducers';
+
+
+
+const reducer = (state , action) => {
+  switch (action.type){
+  
+      default:
+          return state
+  }
+}
+const store = createStore(reducer)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <React.StrictMode>
+        <Provider store={store}>
+        <App />
+        </Provider>
+    </React.StrictMode>
+ 
+  ,
   document.getElementById('root')
 );
 
---
+++ U3/w32d2/redux-moveis-tset/src/reducers/index.js
@@ -0,0 +1,32 @@
+
+import {ADD_MOVIE} from '../actions'
+
+const initialState = {
+    user: {
+        name: 'Dustin'
+      },
+      movies: [
+        'Star Wars',
+        'Lord of the Rings',
+        'Harry Potter'
+      ],
+      todoList: [
+        { task: 'Learn Redux', id: 0, completed: false }
+      ],
+      moviesToWatch: 13
+}
+
+
+
+// export const reducer = (state = initialState, action) => {
+  export const reducer = (state, action) => {
+    switch (action.type){
+        case ADD_MOVIE:
+            return  {
+                ...state,
+                movies: [...state.movies, action.payload]
+            }
+        default:
+            return state
+    }
+}
