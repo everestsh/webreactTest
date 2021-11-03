98e5433097373f1c38f1ecb59d4ec5e871f87ddc
sooof <sooof@me.com>
2021-11-03 16:28:42 -0700

    redux-store-ls_class_test mv reducer() to reducer/index.js

--- U3/w32d3/redux-store-ls_class_test/src/components/Title.js
+++ U3/w32d3/redux-store-ls_class_test/src/components/Title.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import { updateTitle } from '../actions/actions';
 
 class Title extends React.Component {
+    state = {
+      newTitleText: ''
+    };
 
+    handleChanges = e => {
+      this.setState({ [e.target.name]: e.target.value });
+    };
+
+    updateTitle = e => {
+       e.preventDefault();
+       this.props.updateTitle(this.state.newTitleText);
+    };
 
 
     render() {
         return (
         <div className="App">
             <h1>{this.props.title}</h1>
+
+
+        <input
+        type="text"
+        name="newTitleText"
+        value={this.state.newTitleText}
+        onChange={this.handleChanges}
+        />
+        <button onClick={this.updateTitle}>Update title</button>
+    
             
         </div>
         );
@@ -24,5 +46,5 @@ class Title extends React.Component {
 // export default Title;
 export default connect(
     mapStateToProps, 
-    {})(Title);
+    {updateTitle})(Title);
   
