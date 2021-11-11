00698b3e16181066a0aae2b1aa5a18a3ecd85a71
sooof <sooof@me.com>
2021-11-11 14:15:13 -0800

    filter test

--- web-react-test/src/index.js
+++ web-react-test/src/index.js
@@ -12,3 +12,9 @@ function defaultParam(theParam = 'foo') {
     return theParam
   }
   console.log(defaultParam('bar'))
+
+  let arr = [1,2,3,4,5,6,7,8,9]
+
+  console.log(arr)
+ console.log( arr.filter(em => em === 5))
+ console.log( arr.filter(em => em.id === '5'))
