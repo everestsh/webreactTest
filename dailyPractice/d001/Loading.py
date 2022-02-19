d2bc1a2adf77f22f94d83a3a7ca10ec19cfa94a4
Mingjun Geng <sooofeve@gmail.com>
2022-02-19 11:34:44 -0800

    daily Practice For 02182022 01 : js python

---
+++ dailyPractice/d001/Loading.py
@@ -0,0 +1,9 @@
+
+import time
+
+print("---RUN DOT EXAMPLE :Loading ---")
+
+print("Loading",end = "")
+for i in range(20):
+    print(".",end = '',flush = True)
+    time.sleep(0.5)
---
+++ dailyPractice/d001/test.py
@@ -0,0 +1,21 @@
+print("Test")
+print(1)
+print("HelloWorld")
+
+n=0
+print(n),
+while True: 
+    print(n),
+    n+=1
+    if n==10:
+        break
+
+# d = {}
+
+# while True:
+# 	name = input('Input name:')
+# 	if name in d:
+# 		break
+# 	else:
+# 		print('plase input:')
+# 		continue
