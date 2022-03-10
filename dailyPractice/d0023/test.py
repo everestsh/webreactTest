aaff4d078242cd6d739ad053aabad8a5cba79fa9
Mingjun Geng <sooofeve@gmail.com>
2022-03-09 23:06:40 -0800

    daily Practice For 03092022 01 : js python rust

--- dailyPractice/d0023/test.py
+++ dailyPractice/d0023/test.py
@@ -60,26 +60,125 @@ print('----------start------------')
 # n =70
 # width = 3
 # print(Task16(n, width))
-# P4 Task 17
-def Task17(p, n, m):
-    arr = []
-    for x in range(p):
-        for xn in range(n):
-            # print("A")
-            arr.append('A')
-        for xm in range(m):
-            # print("B")
-            arr.append('B')
-    return ''.join(arr)
-    # print("A")
+
+# # P4 Task 17
+# def Task17(p, n, m):
+#     arr = []
+#     for x in range(p):
+#         for xn in range(n):
+#             # print("A")
+#             arr.append('A')
+#         for xm in range(m):
+#             # print("B")
+#             arr.append('B')
+#     return ''.join(arr)
+#     # print("A")
+# p = 2 
+# n = 1 
+# m = 0
+# print(Task17(p, n, m)) 
 
 
-p = 2 
-n = 1 
-m = 0
-print(Task17(p, n, m)) 
+# # U5 W2 P1 Task 1
+# def U5W2P1_Task1(p, n, m):
+#     arr = []
+#     i = 0
+#     for x in range(p):
+#         if(i%3 == 0):
+#             # print("X")
+#             for x in range(p):
+#                 arr.append('X')
+#         else:
+#             for xn in range(n):
+#                 # print("A")
+#                 arr.append('A')
+#             for xm in range(m):
+#                 # print("B")
+#                 arr.append('B')
+#         i += 1 
+#     return ''.join(arr)
+#     # print("A")
+# p = 5
+# n = 1 
+# m = 0
+# print(U5W2P1_Task1(p, n, m))
+
+# U5 W2 P1 Task 2
+# def U5W2P1_Task2(s):
+#     if (s == ''):
+#         return ''
+#     arr = list(s)
+#     # print(len(s))
+#     output = []
+#     for x in arr:
+#         output.insert(0, x)
+#     return "".join(output)
+
+# # s = ''
+# s = "abcdef"
+# print(U5W2P1_Task2(s))
+
+# U5 W2 P1 Task 3
+# def U5W2P1_Task3(s):
+#     output = []
+#     if(s == ''):
+#         return []
+#     print(len(s))
+#     for x in range(len(s)):
+#         output.append(s[x])
+#     return output
+
+# s = "abcdef"
+# s = "@\\_,6"
+# print(U5W2P1_Task3(s))
+
+# U5 W2 P1 Task 4
+# def U5W2P1_Task4(a, v):
+#     count = 0
+#     # print("a =", a)
+#     if(a ==[]):
+#         return 0
+#     else:
+#         for x in a:
+#             if(x == v):
+#                 count += 1
+#     return count
+
+# a = []
+# a = [1, 7, 2, 10, 7]
+# v = 7
+# print(U5W2P1_Task4(a, v))
+
+# U5 W2 P1 Task 5
+# def U5W2P1_Task5(a, b):
+#     # l = len(a)
+#     # print(l)
+#     if ( len(a) != len(b)):
+#         return False
+#     for x in range(len(a)):
+#         if(a[x] != b[x]):
+#             return False
+#     return True
+    
+# a = [1, 7, 2, 10, 7]
+# b = [1, 7, 2, 10, 7]
+# # a = [7, 2, 10, 7]
+# # b = [1, 7, 2, 10, 7]
+# print(U5W2P1_Task5(a, b))
 
 
+# U5 W2 P1 Task 5
+def U5W2P1_Task6(a, x):
+    output = []
+    # print(output)
+    for i in range(len(a)):
+        # print(i)
+        if(a[i] != x ):
+            output.append(a[i])
+    return output
 
+a = [1, 7, 2, 10, 7]
+x = 7
+print(U5W2P1_Task6(a, x))
 
 print('------------end------------')
