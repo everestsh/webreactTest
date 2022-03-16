14d8474ed5fd01fa65cdf41877daf6347fc386e0
Mingjun Geng <sooofeve@gmail.com>
2022-03-15 23:56:42 -0700

    daily Practice For 03115022 03 : js python

--- dailyPractice/d0027/test.py
+++ dailyPractice/d0027/test.py
@@ -20,4 +20,28 @@ print('----------start------------')
 # ################################
 
 
+# print(int('12345'))
+
+# print(int('12345', base=8))
+
+# print(int('12345', base=16)
+
+def int2(x, base=2):
+    return int(x, base)
+
+print(int2('1000000'))
+print(int2('1010101'))
+print( int2('1000000', base=10))
+
+#  python 3 中判断两个对象是否相等
+import operator
+print(operator.eq("hello","name"))
+print(operator.eq("hello","hello"))
+
+
+str1 = "12345"
+list1 = list(str1)
+print(list1)
+
+
 print('------------end------------')
